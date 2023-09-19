d12d8271eed94bc4bc96c0366b6bbb0009c11bec
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-19 18:56:23 +0000

    Add put function to profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile, UserProfile, EmployerProfile
@@ -31,6 +32,8 @@ class EmployerProfileList(APIView):
 
 
 class ProfileDetail(APIView):
+    serializer_class = ProfileSerializer
+
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
@@ -43,8 +46,18 @@ class ProfileDetail(APIView):
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
 
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class UserProfileDetail(APIView):
+    serializer_class = UserProfileSerializer
+
     def get_object(self, pk):
         try:
             profile = UserProfile.objects.get(pk=pk)
@@ -57,8 +70,18 @@ class UserProfileDetail(APIView):
         serializer = UserProfileSerializer(profile)
         return Response(serializer.data)
 
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = UserProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class EmployerProfileDetail(APIView):
+    serializer_class = EmployerProfileSerializer
+
     def get_object(self, pk):
         try:
             profile = EmployerProfile.objects.get(pk=pk)
@@ -70,3 +93,11 @@ class EmployerProfileDetail(APIView):
         profile = self.get_object(pk)
         serializer = EmployerProfileSerializer(profile)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = EmployerProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
