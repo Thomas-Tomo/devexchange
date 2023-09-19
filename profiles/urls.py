0ff40335757fa5cc7b1b4739a93a6bb81785b24a
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-19 18:33:53 +0000

    Add ProfileDetail view

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('profiles/', views.ProfileList.as_view()),
+    path('profiles/<int:pk>/', views.ProfileDetail.as_view()),
     path('user-profiles/', views.UserProfileList.as_view()),
     path('employer-profiles/', views.EmployerProfileList.as_view()),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,12 @@
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile, UserProfile, EmployerProfile
-from .serializers import ProfileSerializer, UserProfileSerializer, EmployerProfileSerializer
+from .serializers import (
+    ProfileSerializer,
+    UserProfileSerializer,
+    EmployerProfileSerializer
+)
 
 
 class ProfileList(APIView):
@@ -22,4 +27,18 @@ class EmployerProfileList(APIView):
     def get(self, request):
         profiles = EmployerProfile.objects.all()
         serializer = EmployerProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
+
+
+class ProfileDetail(APIView):
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
         return Response(serializer.data)
