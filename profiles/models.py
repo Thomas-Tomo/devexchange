e7ec309902a7d1d319df938d423741f32cb2b9d8
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-19 16:30:26 +0000

    Add signal handler func for EmployerProfile

--- profiles/models.py
+++ profiles/models.py
@@ -35,4 +35,10 @@ def create_user_profile(sender, instance, created, **kwargs):
         UserProfile.objects.create(owner=instance)
 
 
+def create_employer_profile(sender, instance, created, **kwargs):
+    if created:
+        EmployerProfile.objects.create(owner=instance)
+
+
 post_save.connect(create_user_profile, sender=User)
+post_save.connect(create_employer_profile, sender=User)
