1f96fc6fc2f8ce26f8248813304269c0328f6368
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-19 17:44:10 +0000

    Add ProfileList view

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
+from .serializers import ProfileSerializer
 
 
 class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
-        return Response(profiles)
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
