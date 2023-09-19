423e27130334407e9be68c3840f542f9643c3ef4
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-19 17:24:24 +0000

    Add ProfileList class to views.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Profile
 
-# Create your views here.
+
+class ProfileList(APIView):
+    def get(self, request):
+        profiles = Profile.objects.all()
+        return Response(profiles)
