66bc3ba2f4c22b9943c861dfbfc9ce61affb4b00
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-19 16:36:01 +0000

    Add Profiles to admin.py

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Profile, UserProfile, EmployerProfile
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(UserProfile)
+admin.site.register(EmployerProfile)
