b3ff018f95d401859ae9e2e5158e321f980f6063
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-19 18:36:15 +0000

    Add UserProfileDetail view

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('profiles/', views.ProfileList.as_view()),
     path('profiles/<int:pk>/', views.ProfileDetail.as_view()),
     path('user-profiles/', views.UserProfileList.as_view()),
+    path('user-profiles/<int:pk>/', views.UserProfileDetail.as_view()),
     path('employer-profiles/', views.EmployerProfileList.as_view()),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -42,3 +42,17 @@ class ProfileDetail(APIView):
         profile = self.get_object(pk)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+
+
+class UserProfileDetail(APIView):
+    def get_object(self, pk):
+        try:
+            profile = UserProfile.objects.get(pk=pk)
+            return profile
+        except UserProfile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = UserProfileSerializer(profile)
+        return Response(serializer.data)
