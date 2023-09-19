a63507103953bfeb360972838db1033c6ca2bb76
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-19 16:25:01 +0000

    Add UserProfile empty class

--- profiles/models.py
+++ profiles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 
@@ -17,3 +18,8 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.owner}'s profile"
+
+
+class UserProfile(Profile):
+    # Add fields specific for user profile here
+    pass
