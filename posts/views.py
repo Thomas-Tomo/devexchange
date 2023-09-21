c51da5abe0db3ab60cf2ca450a0aee1cbc2ff77c
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-21 13:03:47 +0000

    Add permissions check to Post views

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,12 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
+from devexchange.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
 
 
 class PostList(generics.ListCreateAPIView):
     serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Post.objects.all()
 
     def perform_create(self, serializer):
