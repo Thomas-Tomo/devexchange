6a3b1bdd0cb431d9ddde4ebb6dbdcd95256e4037
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-21 12:57:35 +0000

    Add PostList view

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostList(generics.ListCreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
