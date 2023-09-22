cc16af089ed135b6d9d8af264a88bb3beec9534e
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-22 14:41:21 +0000

    Add extra fields to profile serializers

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,15 +1,26 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     user_type = serializers.CharField(source='get_user_type_display')
+    following_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            return following.id if following else None
+        return None
 
     class Meta:
         model = Profile
@@ -23,4 +34,5 @@ class ProfileSerializer(serializers.ModelSerializer):
             'image',
             'is_owner',
             'user_type',
+            'following_id',
         ]
