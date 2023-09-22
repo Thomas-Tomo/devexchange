93a1416f25aba32ba8297e26ace7048e04e13c73
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-22 12:08:09 +0000

    Add CommentLike model

--- likes/models.py
+++ likes/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from posts.models import Post
+from comments.models import Comment
 
 
 class Like(models.Model):
@@ -16,3 +17,18 @@ class Like(models.Model):
 
     def __str__(self):
         return f"{self.owner} {self.post}"
+
+
+class CommentLike(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(
+        Comment, related_name='likes', on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'comment']
+
+    def __str__(self):
+        return f"{self.owner} likes {self.comment}"
