79b65730e151662871cfa7a99c9b5417b113b9b8
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-22 14:44:19 +0000

    Add like_id field to post serializers

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from posts.models import Post
+from likes.models import Like
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -7,6 +8,7 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -20,6 +22,15 @@ class PostSerializer(serializers.ModelSerializer):
                 'Image width larger than 4096px!'
             )
         return value
+
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return like.id if like else None
+        return None
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -39,4 +50,5 @@ class PostSerializer(serializers.ModelSerializer):
             'content',
             'image',
             'image_filter',
+            'like_id',
         ]
