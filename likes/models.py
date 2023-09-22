7a460d033b421260a4c9ddb992cb8a84d6bf814c
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-22 11:42:23 +0000

    Add Like model

--- likes/models.py
+++ likes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
 
-# Create your models here.
+
+class Like(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, related_name='likes', on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'post']
+
+    def __str__(self):
+        return f"{self.owner} {self.post}"
