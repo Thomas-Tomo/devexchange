260f9740d69d2afd6fa5c11ab14fea81554a8992
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-22 11:51:07 +0000

    Add LikeList view

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from devexchange.permissions import IsOwnerOrReadOnly
+from .models import Like
+from .serializers import LikeSerializer
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
