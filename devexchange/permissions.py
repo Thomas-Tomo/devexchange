3d0d6b3231a6372c61f332f88251917b1a863f59
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-23 14:53:37 +0000

    Add permissions for employer type accounts

--- devexchange/permissions.py
+++ devexchange/permissions.py
@@ -6,3 +6,18 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.owner == request.user
+
+
+class IsEmployerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to allow only users with user_type='employer'
+    to create company bio.
+    """
+
+    def has_permission(self, request, view):
+        # Allow all authenticated users, regular and employer to view the list
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Check if the user is authenticated and has user_type='employer'
+        return
+        request.user.is_authenticated and request.user.profile.user_type == 'employer'
