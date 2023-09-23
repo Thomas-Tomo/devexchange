3ef7dd61fea2fd2663d6540e02bc6d08667a53c7
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-23 15:02:15 +0000

    Add UserSkills model

--- user_skills/models.py
+++ user_skills/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserSkill(models.Model):
+    education = models.TextField(blank=True)
+    work_experience = models.TextField(blank=True)
+    skills = models.TextField(blank=True)
+    certifications = models.TextField(blank=True)
+    languages = models.TextField(blank=True)
+    linkedin_profile = models.URLField(max_length=255, blank=True)
+    github_profile = models.URLField(max_length=255, blank=True)
+    portfolio_website = models.URLField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f"{self.owner}'s Skills"
