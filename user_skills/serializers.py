36939ee38478f08347fb5d11639d17aeeee9f547
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-23 15:17:14 +0000

    Add UserSkillSerializer

--- user_skills/serializers.py
+++ user_skills/serializers.py
@@ -5,4 +5,35 @@ from .models import UserSkill
 class UserSkillSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
-    
+    education = serializers.CharField(
+        max_length=255,
+        allow_blank=True,
+        required=False)
+    work_experience = serializers.CharField(
+        max_length=255,
+        allow_blank=True,
+        required=False)
+    skills = serializers.CharField(
+        max_length=255,
+        allow_blank=True,
+        required=False)
+    certifications = serializers.CharField(
+        max_length=255,
+        allow_blank=True,
+        required=False)
+    languages = serializers.CharField(
+        max_length=255,
+        allow_blank=True,
+        required=False)
+    linkedin_profile = serializers.URLField(
+        max_length=255,
+        allow_blank=True,
+        required=False)
+    github_profile = serializers.URLField(
+        max_length=255,
+        allow_blank=True,
+        required=False)
+    portfolio_website = serializers.URLField(
+        max_length=255,
+        allow_blank=True,
+        required=False)
