b31b7a6c8b946ee191296161c2d8bcfe6ed59954
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-23 15:43:34 +0000

    Add permissions for regular account types

--- devexchange/permissions.py
+++ devexchange/permissions.py
@@ -19,5 +19,24 @@ class IsEmployerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         # Check if the user is authenticated and has user_type='employer'
-        return
-        request.user.is_authenticated and request.user.profile.user_type == 'employer'
+        return (
+            request.user.is_authenticated
+            and request.user.profile.user_type == 'employer'
+        )
+
+
+class IsRegularOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to allow only users with user_type='regular'
+    to create company bio.
+    """
+
+    def has_permission(self, request, view):
+        # Allow all authenticated users, regular and employer to view the list
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Check if the user is authenticated and has user_type='regular'
+        return (
+            request.user.is_authenticated
+            and request.user.profile.user_type == 'regular'
+        )
