231d56a0048d6bd80a41227e95c2de285c51e0e0
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-23 14:52:48 +0000

    Add company_bio views

--- company_bio/views.py
+++ company_bio/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from django.db import IntegrityError
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import generics, permissions
+from devexchange.permissions import IsEmployerOrReadOnly, IsOwnerOrReadOnly
+from .models import CompanyBio
+from .serializers import CompanyBioSerializer
 
-# Create your views here.
+
+class CompanyBioList(generics.ListCreateAPIView):
+    queryset = CompanyBio.objects.all()
+    serializer_class = CompanyBioSerializer
+    permission_classes = [IsAuthenticated, IsEmployerOrReadOnly]
+
+    def perform_create(self, serializer):
+        # Automatically set the owner to the current user
+        serializer.save(owner=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        try:
+            return super().create(request, *args, **kwargs)
+        except IntegrityError as e:
+            return Response(
+                {"error": "You already have a company bio."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+class CompanyBioDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CompanyBio.objects.all()
+    serializer_class = CompanyBioSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
