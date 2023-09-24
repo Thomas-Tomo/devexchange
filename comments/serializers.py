e1b5ebdcdec486e8ec4a908d2888937ee2cccbbb
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-24 16:53:50 +0000

    Update comments serializers

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,19 +1,16 @@
 from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
-from .models import Comment
+from .models import Comment, Reply
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    """
-    Serializer for the Comment model
-    Adds three extra fields when returning a list of Comment instances
-    """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     created_at = serializers.SerializerMethodField()
     updated_at = serializers.SerializerMethodField()
+    replies = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -25,18 +22,48 @@ class CommentSerializer(serializers.ModelSerializer):
     def get_updated_at(self, obj):
         return naturaltime(obj.updated_at)
 
+    def get_replies(self, obj):
+        # Retrieve the replies for this comment using Comment model
+        replies_comment_model = Comment.objects.filter(parent_comment=obj)
+
+        # Retrieve the replies for this comment using Reply model
+        replies_reply_model = Reply.objects.filter(parent_comment=obj)
+
+        # Extract the content of each reply from Comment model
+        reply_content_comment_model = [
+            reply.content for reply in replies_comment_model]
+
+        # Extract the content of each reply from Reply model
+        reply_content_reply_model = [
+            reply.content for reply in replies_reply_model]
+
+        # Combine the content from both models
+        all_reply_content = (
+            reply_content_comment_model + reply_content_reply_model)
+
+        return all_reply_content
+
     class Meta:
         model = Comment
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'post', 'content',
+            'post', 'content', 'replies',
         ]
 
 
 class CommentDetailSerializer(CommentSerializer):
-    """
-    Serializer for the Comment model used in Detail view
-    Post is a read only field so that we dont have to set it on each update
-    """
     post = serializers.ReadOnlyField(source='post.id')
+
+
+class ReplySerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
+
+    class Meta:
+        model = Reply
+        fields = [
+            'id', 'owner', 'created_at', 'updated_at',
+            'parent_comment', 'content',
+        ]
