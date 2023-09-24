1be146a1fab8a6f94343cec6892ceabd917946b6
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-24 16:53:02 +0000

    Refactor Comment model to include logic to reply to comments

--- comments/models.py
+++ comments/models.py
@@ -4,11 +4,28 @@ from posts.models import Post
 
 
 class Comment(models.Model):
-    """
-    Comment model, related to User and Post
-    """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    parent_comment = models.ForeignKey(
+        'self', null=True, blank=True, on_delete=models.CASCADE,
+        related_name='replies_to_comment'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.content
+
+
+class Reply(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    parent_comment = models.ForeignKey(
+        Comment, on_delete=models.CASCADE, related_name='replies_to_reply'
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
