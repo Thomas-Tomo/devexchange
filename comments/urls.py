f83c4c2f3f6fdcdcfe60978a0e24411de98090fe
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-24 16:54:51 +0000

    Add urls for comments views

--- comments/urls.py
+++ comments/urls.py
@@ -3,5 +3,11 @@ from comments import views
 
 urlpatterns = [
     path('comments/', views.CommentList.as_view()),
-    path('comments/<int:pk>/', views.CommentDetail.as_view())
+    path('comments/<int:pk>/', views.CommentDetail.as_view()),
+
+    path('replies/<int:parent_comment_id>/', views.ReplyList.as_view()),
+    path(
+        'replies/<int:parent_comment_id>/<int:pk>/',
+        views.ReplyDetail.as_view()),
+
 ]
