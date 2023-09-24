cad0bb856a7cb5e079df5304e0418c59295685aa
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-24 16:54:24 +0000

    Add comment views to handle comments and replies

--- comments/views.py
+++ comments/views.py
@@ -1,8 +1,10 @@
 from rest_framework import generics, permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from devexchange.permissions import IsOwnerOrReadOnly
-from .models import Comment
-from .serializers import CommentSerializer, CommentDetailSerializer
+from .models import Comment, Reply
+from .serializers import (
+    CommentSerializer, CommentDetailSerializer, ReplySerializer
+)
 
 
 class CommentList(generics.ListCreateAPIView):
@@ -24,3 +26,25 @@ class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = CommentDetailSerializer
     queryset = Comment.objects.all()
+
+
+class ReplyList(generics.ListCreateAPIView):
+    serializer_class = ReplySerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        # Assuming you want to filter replies based on the parent comment
+        parent_comment_id = self.kwargs['parent_comment_id']
+        return Reply.objects.filter(parent_comment__id=parent_comment_id)
+
+    def perform_create(self, serializer):
+        # Assuming you want to associate the reply with a parent comment
+        parent_comment_id = self.kwargs['parent_comment_id']
+        parent_comment = Comment.objects.get(pk=parent_comment_id)
+        serializer.save(owner=self.request.user, parent_comment=parent_comment)
+
+
+class ReplyDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = ReplySerializer
+    queryset = Reply.objects.all()
