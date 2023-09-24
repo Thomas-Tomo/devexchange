8ec866e0fd0b334267bfd5b6c393bedae2a45784
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-24 18:58:58 +0000

    Fix local migrations issue

--- job_posts/admin.py
+++ job_posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import JobPost
 
-# Register your models here.
+
+@admin.register(JobPost)
+class JobPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'created_at', 'updated_at', 'is_active')
+    list_filter = ('created_at', 'updated_at', 'is_active')
+    search_fields = (
+        'title', 'owner__username',
+        'location', 'job_type', 'company_name')
