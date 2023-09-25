313e2cc8f706a6399ed2d96bf79a538e1e9dad63
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-25 16:07:03 +0000

    Add JobPostComment model

--- comments/models.py
+++ comments/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from posts.models import Post
+from job_posts.models import JobPost
 
 
 class Comment(models.Model):
@@ -25,6 +26,24 @@ class Reply(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     parent_comment = models.ForeignKey(
         Comment, on_delete=models.CASCADE, related_name='replies_to_reply'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.content
+
+
+class JobPostComment(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    job_post = models.ForeignKey(JobPost, on_delete=models.CASCADE)
+    parent_comment = models.ForeignKey(
+        'self', null=True, blank=True, on_delete=models.CASCADE,
+        related_name='replies_to_comment'
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
