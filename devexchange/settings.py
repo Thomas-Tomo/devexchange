89074b33f91a867b0c09b4eb501661bc6bdbb5ab
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-27 18:05:55 +0000

    Prepare API for deployment

--- devexchange/settings.py
+++ devexchange/settings.py
@@ -37,6 +37,10 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
     'DATETIME_FORMAT': '%d %b %Y',
 }
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
