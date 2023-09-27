31ae5e6c5bf0ffdf395c0846254c46e328bfc99e
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-27 19:41:09 +0000

    Update initial message for react startupp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to DevExchange
         </a>
       </header>
     </div>
