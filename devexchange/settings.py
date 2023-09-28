ba2e772de0a4aacf68fdc4de5a89b87e922cfa6d
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-28 17:48:56 +0000

    Update JWT tokens in settings.py

--- devexchange/settings.py
+++ devexchange/settings.py
@@ -47,7 +47,6 @@ REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
-JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'devexchange.serializers.CurrentUserSerializer'
