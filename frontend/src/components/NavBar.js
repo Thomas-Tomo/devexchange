2892b87fc9dd1843042d8a8bd43287f4530829ad
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-29 18:39:16 +0000

    Add font awesome icons

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -11,10 +11,11 @@ const NavBar = () => {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link>Home</Nav.Link>
-            <Nav.Link>Sign In</Nav.Link>
-            <Nav.Link>Sign Up</Nav.Link>
+          <Nav className="ml-auto text-left">
+            <Nav.Link><i className="fas fa-home"></i>Home</Nav.Link>
+            <Nav.Link><i className="fas fa-suitcase"></i>Jobs</Nav.Link>
+            <Nav.Link><i className="fas fa-sign-in-alt"></i>Sign In</Nav.Link>
+            <Nav.Link><i className="fas fa-user-plus"></i>Sign Up</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
