fd4c9aad191a9ac6ab4101bb0ed89c09284823bd
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-09-29 18:21:13 +0000

    Add logo icon

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/devexchange-logo.png";
 
 const NavBar = () => {
   return (
     <Navbar expand="md" fixed="top" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand>
+          <img src={logo} alt="logo" height="90" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link>Home</Nav.Link>
+            <Nav.Link>Sign In</Nav.Link>
+            <Nav.Link>Sign Up</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
