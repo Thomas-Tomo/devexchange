baa6fd306379db3e94acf42674cb5ee3356b6122
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-10-02 18:49:04 +0000

    Add react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
 import NavBar from "./components/NavBar";
 import styles from "./App.module.css";
 import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
-        <h1>Home</h1>
-        <h1>Sign In</h1>
-        <h1>Sign In</h1>
-        <h1>Sign In</h1>
-        <h1>Sign In</h1>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+        </Switch>
       </Container>
     </div>
   );
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,43 +1,46 @@
 import React from "react";
-import { Navbar, Container, Nav, NavLink } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/devexchange-logo.png";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top" variant="dark">
       <Container>
-        <Navbar.Brand>
-          <img src={logo} alt="logo" height="90" />
-        </Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="90" />
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left mt-3">
-            <NavLink className={styles.NavLink}>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/">
               <i className="fas fa-home"></i>Home
             </NavLink>
-            <NavLink className={styles.NavLink}>
+            <NavLink className={styles.NavLink} to="/">
               <i className="fas fa-suitcase"></i>Jobs
             </NavLink>
-            <NavLink className={styles.NavLink}>
-            <i className="fas fa-list"></i>Feed
+            <NavLink className={styles.NavLink} to="/">
+              <i className="fas fa-list"></i>Feed
             </NavLink>
-            <NavLink className={styles.NavLink}>
-            <i className="fas fa-thumbs-up"></i>Liked
+            <NavLink className={styles.NavLink} to="/">
+              <i className="fas fa-thumbs-up"></i>Liked
             </NavLink>
-            <NavLink className={styles.NavLink}>
-            <i className="fas fa-bookmark"></i>Saved Jobs
+            <NavLink className={styles.NavLink} to="/">
+              <i className="fas fa-bookmark"></i>Saved Jobs
             </NavLink>
-            <NavLink className={styles.NavLink}>
-            <i className="fas fa-user"></i>Profile
+            <NavLink className={styles.NavLink} to="/">
+              <i className="fas fa-user"></i>Profile
             </NavLink>
-            <NavLink className={styles.NavLink}>
+            <NavLink className={styles.NavLink} to="/">
               <i className="fas fa-sign-in-alt"></i>Sign In
             </NavLink>
-            <NavLink className={styles.NavLink}>
-            <i className="fas fa-sign-out-alt"></i>Sign Out
+            <NavLink className={styles.NavLink} to="/">
+              <i className="fas fa-sign-out-alt"></i>Sign Out
             </NavLink>
-            <NavLink className={styles.NavLink}>
+            <NavLink className={styles.NavLink} to="/">
               <i className="fas fa-user-plus"></i>Sign Up
             </NavLink>
           </Nav>
