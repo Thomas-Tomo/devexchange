b0061a1a22abcdf43b23364fd6cdc0236530b09b
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-10-03 19:53:28 +0000

    Add SignUpForm state handling

--- frontend/src/pages/auth/SignUpForm.js
+++ frontend/src/pages/auth/SignUpForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
@@ -7,9 +7,24 @@ import btnStyles from "../../styles/Button.module.css";
 import { Form, Button, Col, Row, Container } from "react-bootstrap";
 
 const SignUpForm = () => {
+  const [signUpData, setSignUpData] = useState({
+    username: "",
+    password1: "",
+    password2: "",
+  });
+
+  const { username, password1, password2 } = signUpData;
+
+  const handleChange = (event) => {
+    setSignUpData({
+        ...signUpData,
+        [event.target.name]: event.target.value,
+    })
+  }
+
   return (
     <Row className={`${styles.Row} m-2`}>
-      <Col className="m-auto py-2 p-md-2" md={8} >
+      <Col className="m-auto py-2 p-md-2" md={8}>
         <Container className="p-4">
           <h1 className={styles.Header}>Create an account</h1>
 
@@ -20,6 +35,9 @@ const SignUpForm = () => {
                 className={styles.Input}
                 type="text"
                 placeholder="Enter Username"
+                name="username"
+                value={username}
+                onChange={handleChange}
               />
             </Form.Group>
 
@@ -29,6 +47,9 @@ const SignUpForm = () => {
                 className={styles.Input}
                 type="password"
                 placeholder="Password"
+                name="password1"
+                value={password1}
+                onChange={handleChange}
               />
             </Form.Group>
             <Form.Group controlId="password2">
@@ -37,15 +58,24 @@ const SignUpForm = () => {
                 className={styles.Input}
                 type="password"
                 placeholder="Confirm Password"
+                name="password2"
+                value={password2}
+                onChange={handleChange}
               />
             </Form.Group>
-            <Button className={`${btnStyles.Button} ${btnStyles.Wide}`} type="submit">
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Wide}`}
+              type="submit"
+            >
               Sign Up
             </Button>
           </Form>
         </Container>
         <Container className="mt-3 mb-2 text-center">
-          <Link className={`${styles.Link} ${styles.ContainerLink}`} to="/signin">
+          <Link
+            className={`${styles.Link} ${styles.ContainerLink}`}
+            to="/signin"
+          >
             Already have an account? <span>Sign in</span>
           </Link>
         </Container>
