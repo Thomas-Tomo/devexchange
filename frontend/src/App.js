08b07b290b3c06d3fdcde4582eea7bf8e9968047
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-10-03 18:34:32 +0000

    Add basic design to Sign Up form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import NavBar from "./components/NavBar";
 import styles from "./App.module.css";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
+import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
           <Route exact path="/profile" render={() => <h1>Profile</h1>} />
           <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
           <Route exact path="/signout" render={() => <h1>Sign Out</h1>} />
-          <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
