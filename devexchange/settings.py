5e094875225e3aa1429800148ac34a12a1d98cfa
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-10-04 20:02:35 +0000

    Add corsheaders middleware

--- devexchange/settings.py
+++ devexchange/settings.py
@@ -105,6 +105,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
