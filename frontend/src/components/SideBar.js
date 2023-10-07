5da73f875c9d78ac0c3c115095054c84fa9adbc5
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-10-07 11:06:59 +0000

    Add Sidebar.js that holds Add Post, Add Job links

---
+++ frontend/src/components/SideBar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Col, NavLink } from "react-bootstrap";
+import styles from "../styles/SideBar.module.css";
+
+const SideBar = () => {
+  return (
+    <>
+      {/* Left Column */}
+      <Col md={2} className={styles.SideBar}>
+        <div className={styles.CustomCard}>
+          <div className={styles.CardBody}>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/posts/create"
+            >
+              <i className="fas fa-pencil-alt"></i>Add Post
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/posts/create"
+            >
+              <i className="fas fa-code"></i> Add Job
+            </NavLink>
+          </div>
+        </div>
+        <div className={`mt-3 ${styles.CustomCard}`}>
+          <div>Copyright &copy; Thomas-Tomo Domitrovic</div>
+        </div>
+      </Col>
+    </>
+  );
+};
+
+export default SideBar;
