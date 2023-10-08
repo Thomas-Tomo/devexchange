da4d2664f22e9f63240d1263119315ef1deef9a9
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-10-08 10:23:56 +0000

    Update PostCreateForm style

--- frontend/src/pages/posts/PostCreateForm.js
+++ frontend/src/pages/posts/PostCreateForm.js
@@ -1,45 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import styles from "../../styles/PostCreateEditForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 
 function PostCreateForm() {
-  const [image, setImage] = useState(null); // State to store the selected image
-  const [content, setContent] = useState(""); // State to store post content
-  const [errors, setErrors] = useState({});
-
-  const handleImageChange = (e) => {
-    const selectedImage = e.target.files[0];
-    setImage(selectedImage);
-  };
-
-  const handleCreatePost = (e) => {
-    e.preventDefault();
-
-    // Validate form fields (you can add your validation logic here)
-    if (!image) {
-      setErrors({ image: "Please select an image" });
-      return;
-    }
-    if (!content.trim()) {
-      setErrors({ content: "Please enter post content" });
-      return;
-    }
-
-    // Reset errors
-    setErrors({});
-
-    // Add your logic for creating a post here
-    // For example, you can send a POST request to your API
-    // After creating the post, you can navigate to the post detail page
-    // For now, let's just clear the form fields
-    setImage(null);
-    setContent("");
-  };
-
   return (
-    <Form>
+    <Form className={styles.Form}>
       <Form.Group controlId="image">
         <Form.Label className={styles.FormLabel}>Upload an Image</Form.Label>
         <label htmlFor="file-input" className={styles.CustomFileInput}>
@@ -49,33 +16,18 @@ function PostCreateForm() {
           id="file-input"
           type="file"
           accept="image/*"
-          onChange={handleImageChange}
           className={styles.FileInput}
         />
+      </Form.Group>
 
-        {errors.image && <div className="text-danger">{errors.image}</div>}
-        {image && (
-          <img
-            src={URL.createObjectURL(image)} // Display the selected image
-            alt="Selected"
-            className={styles.SelectedImage}
-          />
-        )}
+      <Form.Group controlId="content">
+        <Form.Label className={styles.FormLabel}>Post Title</Form.Label>
+        <Form.Control className={styles.FormControl} as="textarea" rows={1} />
       </Form.Group>
 
       <Form.Group controlId="content">
         <Form.Label className={styles.FormLabel}>Post Content</Form.Label>
-        <Form.Control
-          as="textarea"
-          rows={3}
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        />
-        {errors.content && (
-          <div className={`text-danger ${styles.FormControl}`}>
-            {errors.content}
-          </div>
-        )}
+        <Form.Control as="textarea" rows={3} />
       </Form.Group>
 
       <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
