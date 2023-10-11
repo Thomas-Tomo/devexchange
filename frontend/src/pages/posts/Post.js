b18e92cd8d6074ae9a818cf6c420ae25c3325053
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-10-11 18:57:43 +0000

    Fix Unlike request post endpoint

--- frontend/src/pages/posts/Post.js
+++ frontend/src/pages/posts/Post.js
@@ -44,7 +44,7 @@ const Post = (props) => {
 
   const handleUnlike = async () => {
     try {
-      const { data } = await axiosRes.post("/likes/", { post: id });
+      await axiosRes.delete(`/likes/${like_id}/`);
       setPosts((prevPosts) => ({
         ...prevPosts,
         results: prevPosts.results.map((post) => {
@@ -54,7 +54,7 @@ const Post = (props) => {
         }),
       }));
     } catch (err) {
-      console.log(err);
+       console.log(err);
     }
   };
 
