4d6c47c9a79eb3b27b5820e0f86829e2f584fa24
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-10-11 20:06:19 +0000

    Fix sidebar displa for small screens

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -100,7 +100,7 @@ const NavBar = () => {
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
-            <img src={logo} alt="logo" height="90" />
+            <img src={logo} alt="logo" height={70} />
           </Navbar.Brand>
         </NavLink>
 
