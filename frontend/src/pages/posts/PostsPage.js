f663ff7e06a25cba7ca23cb9eb61efd2e099eace
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-10-11 17:27:27 +0000

    Add boilerplate for PostsPage.js

---
+++ frontend/src/pages/posts/PostsPage.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import styles from "../../styles/PostsPage.module.css";
+
+function PostsPage() {
+  
+  return (
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <p>Popular profiles mobile</p>
+        <p>List of posts here</p>
+      </Col>
+      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+        <p>Popular profiles for desktop</p>
+      </Col>
+    </Row>
+  );
+}
+
+export default PostsPage;
