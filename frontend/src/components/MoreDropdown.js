586ab1059712fad606e5da9dd27c5f7c881642af
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-10-17 15:02:09 +0000

    Update Dropdown menu with with buttons

--- frontend/src/components/MoreDropdown.js
+++ frontend/src/components/MoreDropdown.js
@@ -1,37 +1,44 @@
 import React from "react";
 import Dropdown from "react-bootstrap/Dropdown";
-import styles from "../styles/MoreDropdown.module.css"
-
+import styles from "../styles/MoreDropdown.module.css";
 
 // The forwardRef is important!!
 // Dropdown needs access to the DOM node in order to position the Menu
-const ThreeDots = React.forwardRef(({  onClick }, ref) => (
-    <i
+const ThreeDots = React.forwardRef(({ onClick }, ref) => (
+  <i
     className="fas fa-ellipsis-h"
-      ref={ref}
-      onClick={(e) => {
-        e.preventDefault();
-        onClick(e);
-      }}
-    />
-  ));
+    ref={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+  />
+));
 
-  export const MoreDropdown = () => {
-    return (
-        <Dropdown>
-        <Dropdown.Toggle as={ThreeDots} id="dropdown-custom-components">
-          Custom toggle
-        </Dropdown.Toggle>
-    
-        <Dropdown.Menu popperConfig={{ strategy: "fixed" }}>
-          <Dropdown.Item eventKey="1">Red</Dropdown.Item>
-          <Dropdown.Item eventKey="2">Blue</Dropdown.Item>
-          <Dropdown.Item eventKey="3" active>
-            Orange
-          </Dropdown.Item>
-          <Dropdown.Item eventKey="1">Red-Orange</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-    )
-  }
- 
+export const MoreDropdown = () => {
+  return (
+    <Dropdown className="ml-auto" drop="left">
+      <Dropdown.Toggle as={ThreeDots} />
+
+      <Dropdown.Menu
+        className={`text-center ${styles.DropdownMenu}`}
+        popperConfig={{ strategy: "fixed" }}
+      >
+        <Dropdown.Item
+          className={styles.DropdownItem}
+          onClick={() => {}}
+          aria-label="edit"
+        >
+          <i className="far fa-edit" />
+        </Dropdown.Item>
+        <Dropdown.Item
+          className={styles.DropdownItem}
+          onClick={() => {}}
+          aria-label="delete"
+        >
+          <i className="fas fa-trash" />
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
