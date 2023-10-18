4472443244f9cb276fae01a6e747018d11a63d4d
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-10-18 14:41:11 +0000

    Fix comment container with comment elements

--- frontend/src/pages/posts/PostPage.js
+++ frontend/src/pages/posts/PostPage.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import appStyles from "../../App.module.css";
+import styles from "../../styles/Post.module.css";
 import { useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function PostPage() {
   const { id } = useParams();
   const [post, setPost] = useState({ results: [] });
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
@@ -45,7 +51,19 @@ function PostPage() {
         <Col md={8}>
           {/* Post Component */}
           <Post {...post.results[0]} setPosts={setPost} postPage />
-          <Container>Comments</Container>
+          <Container className={styles.CommentCard}>
+            {currentUser ? (
+              <CommentCreateForm
+                profile_id={currentUser.profile_id}
+                profileImage={profile_image}
+                post={id}
+                setPost={setPost}
+                setComments={setComments}
+              />
+            ) : comments.results.length ? (
+              "Comments"
+            ) : null}
+          </Container>
           <Container>Replies to comments</Container>
         </Col>
         <Col lg={4} className="d-none d-lg-block">
