ccdd0a1129c3ee59fd9cb3d4bab3e2d223bd67ba
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-10-19 16:24:24 +0000

    Add comments request and display them on the post

--- frontend/src/pages/posts/PostPage.js
+++ frontend/src/pages/posts/PostPage.js
@@ -18,10 +18,12 @@ function PostPage() {
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: post }] = await Promise.all([
+        const [{ data: post }, { data: comments }] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comments/?post=${id}`),
         ]);
         setPost({ results: [post] });
+        setComments(comments);
       } catch (err) {
         console.log(err);
       }
@@ -63,6 +65,17 @@ function PostPage() {
             ) : comments.results.length ? (
               "Comments"
             ) : null}
+            {comments.results.length ? (
+              comments.results.map((comment) => (
+                <p key={comment.id}>
+                  {comment.owner} : {comment.content}
+                </p>
+              ))
+            ) : currentUser ? (
+              <span>No comments yet, be first to comment!</span>
+            ) : (
+              <span>No comments... yet</span>
+            )}
           </Container>
           <Container>Replies to comments</Container>
         </Col>
