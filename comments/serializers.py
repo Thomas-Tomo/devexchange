a91b30ddfb22cbabafa19b9bd7be903b9852b3ae
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-10-22 13:14:10 +0000

    Refactor timestamp in reply serlializer

--- comments/serializers.py
+++ comments/serializers.py
@@ -71,8 +71,14 @@ class CommentDetailSerializer(CommentSerializer):
 
 class ReplySerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    created_at = serializers.DateTimeField(read_only=True)
-    updated_at = serializers.DateTimeField(read_only=True)
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
 
     class Meta:
         model = Reply
