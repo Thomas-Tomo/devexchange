27deebae9bc804d06f67b721596b4e591f4921e1
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-10-25 17:18:37 +0000

    Add handleMount function to fetch most followed profiles

--- frontend/src/pages/profiles/PopularProfiles.js
+++ frontend/src/pages/profiles/PopularProfiles.js
@@ -1,13 +1,38 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React, { useEffect } from "react";
+import { Container } from "react-bootstrap";
 import styles from "../../styles/Post.module.css";
 
 const PopularProfiles = () => {
+  const [profileData, setProfileData] = useState({
+    // use the pageProfile later
+    pageProfile: { results: [] },
+    popularProfiles: { results: [] },
+  });
+
+  const { popularProfiles } = profileData;
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          "/profiles/?ordering=-followers_count"
+        );
+        setProfileData((prevState) => ({
+          ...prevState,
+          popularProfiles: data,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    handleMount()
+  });
+
   return (
     <Container className={styles.PostCard}>
-        <p>Most followed profiles</p>
+      <p>Most followed profiles</p>
     </Container>
-  )
-}
+  );
+};
 
-export default PopularProfiles
+export default PopularProfiles;
