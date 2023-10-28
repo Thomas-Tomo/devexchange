95e2f8ea8fefcc9b476c7edf940783382ea28b67
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-10-28 17:13:07 +0000

    Add profile page image data

--- frontend/src/pages/profiles/ProfilePage.js
+++ frontend/src/pages/profiles/ProfilePage.js
@@ -11,20 +11,42 @@ import btnStyles from "../../styles/Button.module.css";
 
 import PopularProfiles from "./PopularProfiles";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { useParams } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useProfileData, useSetProfileData } from "../../contexts/ProfileDataContext";
+import { Button, Image } from "react-bootstrap";
 
 function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
   const currentUser = useCurrentUser();
+  const {id} = useParams();
+  const setProfileData = useSetProfileData();
+  const {pageProfile} = useProfileData();
+  const [profile] = pageProfile.results;
 
   useEffect(() => {
-      setHasLoaded(true);
-  }, [])
+    const fetchData = async () => {
+        try {
+            const [{data: pageProfile}] = await Promise.all([
+                axiosReq.get(`/profiles/${id}/`)
+            ])
+            setProfileData(prevState => ({
+                ...prevState,
+                pageProfile: {results: [pageProfile]}
+            }))
+            setHasLoaded(true);
+        } catch(err) {
+            console.log(err)
+        }
+    }
+      fetchData();
+  }, [id, setProfileData])
 
   const mainProfile = (
     <>
       <Row noGutters className="px-3 text-center">
         <Col lg={3} className="text-lg-left">
-          <p>Image</p>
+          <Image className={styles.ProfileImage} roundedCircle src={profile?.image} />
         </Col>
         <Col lg={6}>
           <h3 className="m-2">Profile username</h3>
