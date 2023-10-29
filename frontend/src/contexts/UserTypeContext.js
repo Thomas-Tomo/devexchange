f5ec9b83610bcef96d12ccfae3832f368ea38f53
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-10-29 11:26:53 +0000

    Add UserTypeContext to determine regular or employer

---
+++ frontend/src/contexts/UserTypeContext.js
@@ -0,0 +1,36 @@
+// UserTypeContext.js
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+import { useCurrentUser } from "../contexts/CurrentUserContext";
+
+const UserTypeContext = createContext();
+
+export const useUserType = () => useContext(UserTypeContext);
+
+export const UserTypeProvider = ({ children }) => {
+  const [userType, setUserType] = useState(null);
+  const currentUser = useCurrentUser();
+
+  useEffect(() => {
+    const fetchProfileData = async () => {
+      try {
+        const response = await axios.get(`/profiles/${currentUser?.profile_id}/`);
+        const profileData = response.data;
+        const fetchedUserType = profileData.user_type;
+        setUserType(fetchedUserType);
+      } catch (error) {
+        console.error('Error fetching profile data:', error);
+      }
+    };
+
+    if (currentUser && currentUser.profile_id) {
+      fetchProfileData();
+    }
+  }, [currentUser, currentUser?.profile_id]);
+
+  return (
+    <UserTypeContext.Provider value={userType}>
+      {children}
+    </UserTypeContext.Provider>
+  );
+};
