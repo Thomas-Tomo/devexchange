88076f4b10c1538846c5e902db359d920aba7bf1
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-10-31 19:27:49 +0000

    Fix typo inside utils followHelper function

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,36 +1,37 @@
-import { axiosReq } from "../api/axiosDefaults"
+import { axiosReq } from "../api/axiosDefaults";
 
 export const fetchMoreData = async (resource, setResource) => {
-    try {
-        const { data } = await axiosReq.get(resource.next)
-        setResource((prevResource) => ({
-            ...prevResource,
-            next:data.next,
-            results: data.results.reduce((acc, cur) => {
-                return acc.some((accResult) => accResult.id === cur.id) ? acc : [...acc, cur]
-            }, prevResource.results),
-        }))
-    } catch(err){
-
-    }
-}
+  try {
+    const { data } = await axiosReq.get(resource.next);
+    setResource((prevResource) => ({
+      ...prevResource,
+      next: data.next,
+      results: data.results.reduce((acc, cur) => {
+        return acc.some((accResult) => accResult.id === cur.id)
+          ? acc
+          : [...acc, cur];
+      }, prevResource.results),
+    }));
+  } catch (err) {}
+};
 
 export const followHelper = (profile, clickedProfile, following_id) => {
-    return profile.id === clickedProfile.id
-      ? // This is the profile I clicked on,
-        // update its followers count and set its following id
-        {
-          ...profile,
-          followers_count: profile.followers_count + 1,
-          following_id,
-        }
-      : profile.is_owner
-      ? // This is the profile of the logged in user
-        // update its following count
-        {
-          ...profile, followers_count: profile.following_count + 1
-        }
-      : // this is not the profile the user clicked on or the profile
-        // the user owns, so just return it unchanged
-        profile;
-  }
+  return profile.id === clickedProfile.id
+    ? // This is the profile I clicked on,
+      // update its followers count and set its following id
+      {
+        ...profile,
+        followers_count: profile.followers_count + 1,
+        following_id,
+      }
+    : profile.is_owner
+    ? // This is the profile of the logged in user
+      // update its following count
+      {
+        ...profile,
+        following_count: profile.following_count + 1,
+      }
+    : // this is not the profile the user clicked on or the profile
+      // the user owns, so just return it unchanged
+      profile;
+};
