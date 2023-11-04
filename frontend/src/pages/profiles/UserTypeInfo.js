2539d06508c65e82f02d3569039fb893f92f3cd2
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-11-04 17:49:44 +0000

    Add user account information component

---
+++ frontend/src/pages/profiles/UserTypeInfo.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const UserTypeInfo = () => {
+  return (
+    <div className="d-flex justify-content-end">
+      <div>
+        <h5 className="text-center">Upgrade to an Employer Account</h5>
+        <ul>
+          <li>
+            You can upgrade your account to an employer account to create job
+            posts and manage your company details.
+          </li>
+          <li>
+            If you are interested in creating job posts and showcasing your
+            company, go to your profile edit form.
+          </li>
+          <li>
+            If you have already done that you can safely ignore this information
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default UserTypeInfo;
