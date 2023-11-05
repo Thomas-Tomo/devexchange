edf56697bcbe3d2eafe625fab168c1b41bf3e895
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-11-05 14:43:07 +0000

    Fix job posts count profile queryset

--- profiles/views.py
+++ profiles/views.py
@@ -11,7 +11,7 @@ class ProfileList(generics.ListAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.annotate(
         posts_count=Count('owner__post', distinct=True),
-        job_posts_count=Count('owner__post', distinct=True),
+        job_posts_count=Count('owner__jobpost', distinct=True),
         followers_count=Count('owner__followed', distinct=True),
         following_count=Count('owner__following', distinct=True)
         ).order_by('-created_at')
