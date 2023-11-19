07482d444ff91df6d67e5d50e853f438b9d9a122
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-11-19 17:33:36 +0000

    Add context for recent jobs

---
+++ frontend/src/contexts/RecentJobsContext.js
@@ -0,0 +1,31 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { axiosReq } from "../api/axiosDefaults";
+
+const RecentJobsContext = createContext();
+
+export const useRecentJobs = () => useContext(RecentJobsContext);
+
+export const RecentJobsProvider = ({ children }) => {
+  const [recentJobs, setRecentJobs] = useState([]);
+
+  useEffect(() => {
+    const fetchRecentJobs = async () => {
+      try {
+        // Fetch recent job posts from API or database sorted by creation time
+        const { data } = await axiosReq.get("/job-posts/?ordering=-created_at");
+
+        setRecentJobs(data); // Set the fetched recent jobs into state
+      } catch (error) {
+        console.error("Error fetching recent jobs:", error);
+      }
+    };
+
+    fetchRecentJobs();
+  }, []);
+
+  return (
+    <RecentJobsContext.Provider value={recentJobs}>
+      {children}
+    </RecentJobsContext.Provider>
+  );
+};
