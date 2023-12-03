c0c52d438e40780f39ec433b5d8d1e9240fd7eab
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-12-03 17:20:53 +0000

    Update settings for Heroku deployment

--- devexchange/settings.py
+++ devexchange/settings.py
@@ -110,7 +110,7 @@ SITE_ID = 1
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,8 +124,7 @@ ROOT_URLCONF = 'devexchange.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        # 'DIRS': [os.path.join(BASE_DIR, 'staticfiles', 'build')],
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'staticfiles', 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -194,8 +193,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
-# WHITENOISE_ROOT = BASE_DIR / 'staticfiles' / 'build'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+WHITENOISE_ROOT = BASE_DIR / 'staticfiles' / 'build'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,24 +1,6 @@
-// import axios from "axios";
-
-// axios.defaults.baseURL = "/api";
-// axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
-// axios.defaults.withCredentials = true;
-
-// export const axiosReq = axios.create();
-// export const axiosRes = axios.create();
-
 import axios from "axios";
 
-// IMPORTANT!!
-// Because this React app is running in the same workspace as the API,
-
-// there is no need to set a separate baseURL until you reach deployment.
-
-// Setting a baseURL before you reach deployment will cause errors
-
-// axios.defaults.xsrfCookieName = 'csrftoken';
-// axios.defaults.xsrfHeaderName = 'X-CSRFToken';
-
+axios.defaults.baseURL = "/api";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
